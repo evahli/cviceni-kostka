1c60ea84885cae0e4f455ddec7e96a11cc7595fb
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-04-07 10:45:46 +0200

    upload solution

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+/* Doplňte do stránky JavaScriptový program, který zařídí, že když uživatel zmáčkne na stránce libovolnou klávesu, na kostce se zobrazí číslo o jedna vyšší. Po šestce zobrazte opět jedničku.
 
+Nápověda: Rozumným postupem je vytvořit si proměnnou side, která bude obsahovat číslo strany kostky. Tuto proměnnou můžete vždy při stisknutí klávesy navýšit o jedna a z této hodnoty zkonstruovat řetězec pro atribut src.*/
+
+let side = 1
+
+const hodit = () => {
+  side = side + 1;
+  if (side > 6) {
+    side = 1;
+  }
+  const kostkaElement = document.querySelector('.dice');
+  kostkaElement.src = "img/side" + side + ".svg"
+}
+
+
+document.addEventListener("keydown", hodit)
